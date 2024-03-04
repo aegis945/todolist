42dcc1b49a37771865da27bdef28950b14489aa2
aegis945 <artiom.lekincev@gmail.com>
2024-03-05 01:37:07 +0200

    add fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        body: ['Montserrat', 'Roboto', 'Josefin Sans', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }
